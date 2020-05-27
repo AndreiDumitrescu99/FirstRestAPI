ac798d7e8c6a4d24607cd021558b7898070882a7
adrianatudor <adriana.tudor2898@gmail.com>
2020-05-27 20:00:14 +0300

    Flask vede css

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 app = Flask(__name__)
+
+username = []
+passwords = []
 
 @app.route('/')
 def home():
-    return render_template('my.html')
+    return render_template("my.html")
 
-def contact():
+def do_sign_up(new_user, new_pass):
+    semafor = 1
+    for user in username:
+        if new_user == user:
+            print("Exista deja un utilizator cu acest username")
+            semafor = 0
+    if semafor == 1:
+        username.append(new_user)
+        passwords.append(new_pass)
+        print("Utilizator creat cu succes")
+
+
+@app.route('/', methods=['POST'])
+def sign_up():
     if request.method == 'POST':
-        if request.form['sign_up'] == 'Sign up':
-            render_template('contact.html')
-        else:
-            pass
-    elif request.method == 'GET':
-        return render_template('contact.html', form=form)
+        new_user = request.form['username']
+        new_pass = request.form['password']
+        do_sign_up(new_user, new_pass)
+        return render_template("my.html")
+
 
 if __name__ == '__main__':
     app.run();
