af8ef3a205504e9e43d155e515419e0d35bde8e0
AndreiDumitrescu99 <andreidumitrescu99@gmail.com>
2020-05-27 18:18:45 +0300

    first try

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('my.html')
+    return render_template("my.html")
 
-if __name__ == '__main__'
+def do_sign_up():
+    print("Interceptat")
+
+@app.route('/', methods=['POST'])
+def sign_up():
+    if request.method == 'POST':
+        return do_sign_up()
+
+
+if __name__ == '__main__':
     app.run();
