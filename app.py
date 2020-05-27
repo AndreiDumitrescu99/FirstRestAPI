13935d04be3b54c6c8d9fcefe35ee9df573ec173
adrianatudor <adriana.tudor2898@gmail.com>
2020-05-27 18:41:48 +0300

    Starting from the bottom

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('my.html')
 
-if __name__ == '__main__'
+def contact():
+    if request.method == 'POST':
+        if request.form['sign_up'] == 'Sign up':
+            render_template('contact.html')
+        else:
+            pass
+    elif request.method == 'GET':
+        return render_template('contact.html', form=form)
+
+if __name__ == '__main__':
     app.run();
