cc9be1df2ab4442b6fb5c4a0eef52b9e3283df61
AndreiDumitrescu99 <andreidumitrescu99@gmail.com>
2020-05-27 19:26:35 +0300

    mult mai mult

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 app = Flask(__name__)
+
+username = []
+passwords = []
 
 @app.route('/')
 def home():
     return render_template("my.html")
 
-def do_sign_up():
-    print("Interceptat")
+def do_sign_up(new_user, new_pass):
+    semafor = 1
+    for user in username:
+        if new_user == user:
+            print("Exista deja un utilizator cu acest username")
+            semafor = 0
+    if semafor == 1:
+        username.append(new_user)
+        passwords.append(new_pass)
+        print("Utilizator creat cu succes")
+
 
 @app.route('/', methods=['POST'])
 def sign_up():
     if request.method == 'POST':
-        do_sign_up()
+        new_user = request.form['username']
+        new_pass = request.form['password']
+        do_sign_up(new_user, new_pass)
         return render_template("my.html")
 
 
