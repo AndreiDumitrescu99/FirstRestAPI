ccb9c5841b39ddd4e930a33a5bfdd5fc779a4e53
AndreiDumitrescu99 <andreidumitrescu99@gmail.com>
2020-05-28 15:27:17 +0300

    am adaugat remove

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
-username = []
+usernames = []
 passwords = []
 error = None
 myfile = open("state.txt", 'r+')
@@ -10,15 +10,15 @@ for line in myfile:
     i = 1
     for word in line.split():
         if i == 1:
-            username.append(word)
+            usernames.append(word)
         else:
             passwords.append(word)
         i = i + 1
 myfile.seek(0, os.SEEK_END)
 
 def check_for_user(user, passd):
-    for i in range(len(username)):
-        if username[i] == user:
+    for i in range(len(usernames)):
+        if usernames[i] == user:
             if passwords[i] == passd:
                 return 1
             else:
@@ -39,13 +39,13 @@ def home():
 
 def do_sign_up(new_user, new_pass):
     semafor = 1
-    for user in username:
+    for user in usernames:
         if new_user == user:
             error = 'Already exists'
             print("Exista deja un utilizator cu acest username")
             semafor = 0
     if semafor == 1:
-        username.append(new_user)
+        usernames.append(new_user)
         passwords.append(new_pass)
         myfile.write(new_user)
         myfile.write(" ")
@@ -63,13 +63,41 @@ def sign_up():
         do_sign_up(new_user, new_pass)
         return render_template("my.html", error = error)
 
-@app.route('/user/<username>')
+@app.route('/user/<username>', methods=["GET", "DELETE"])
 def profile(username):
+    print("Buna BAU BAU")
+    if request.method == 'GET':
+        if username not in usernames:
+            return redirect('/')
+    if request.method == 'DELETE':
+        return redirect('/user/delete')
     return render_template("user.html", value=username)
 
 @app.route('/error')
 def error():
     return ('Wrong username or password')
+
+def delete_user(user):
+    semafor = 0
+    for i in range(len(usernames)):
+        if usernames[i] == user:
+            passd = passwords[i]
+    usernames.remove(user)
+    passwords.remove(passd)
+    myfile = open("state.txt", 'w+')
+    for i in range(len(usernames)):
+        myfile.write(usernames[i])
+        myfile.write(" ")
+        myfile.write(passwords[i])
+        myfile.write("\n")
+
+@app.route('/user/delete/', methods=["DELETE"])
+def onsuccesfuldelete():
+    print("Buna")
+    user = request.form['username']
+    delete_user(user)
+    return("I tried")
+
 
 
 if __name__ == '__main__':
